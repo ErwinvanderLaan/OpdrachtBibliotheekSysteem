fa8ab2d6a9b09333b2abd0f37ef904518d957173
ErwinvanderLaan <erwinvanderlaan@gmail.com>
2024-10-21 15:04:00 +0200

    Worked on the 'book respository' class.

--- src/main/java/Magazine/Magazine.java
+++ src/main/java/Magazine/Magazine.java
@@ -1,9 +1,8 @@
-package Magazine;
+package magazine;
 
-import java.time.LocalDate;
 import library.Item;
 
-class Magazine extends Item {
+public class Magazine extends Item {
 
   private String issn;
   private int issueNumber;
@@ -11,25 +10,8 @@ class Magazine extends Item {
 
   // The class constructor:
   public Magazine( long id, String title, String author, int pages, String issn, int issueNumber) {
-   super(id, title, author, pages);
+    super(id, title, author, pages);
     this.issn = issn;
-    this.issueNumber = issueNumber;
-  }
-
-  // Getters & Setters
-  public String getIssn() {
-    return issn;
-  }
-
-  public int getIssueNumber() {
-    return issueNumber;
-  }
-
-  public void setIssn(String issn) {
-    this.issn = issn;
-  }
-
-  public void setIssueNumber(int issueNumber) {
     this.issueNumber = issueNumber;
   }
 
