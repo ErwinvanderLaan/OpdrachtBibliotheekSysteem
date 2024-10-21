3823c8b0f95af77548c2b80950f264b8ad151d69
ErwinvanderLaan <erwinvanderlaan@gmail.com>
2024-10-21 17:12:53 +0200

    Worked out 'main' class.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,38 @@
 package main;
 
+import book.BookController;
+import magazine.MagazineController;
 import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
 
+//Shows a menu where the user can choose between working with books or magazines
+    System.out.println("""
+        Choose an option:
+        1. Work with books
+        2. Work with magazines
+        3. Type 'q' to quit.
+        """);
+
+    String choice = askForInput("Choose an option:");
+
+    // A switch for each of the three choices.
+    switch (choice) {
+      case "1":
+        BookController.showMenu();
+        break;
+
+      case "2":
+        MagazineController.showMenu();
+        break;
+
+      case "q":
+        break;
     }
+
+  }
 
   // Scanner method to ask for user input.
   public static String askForInput (String question){
