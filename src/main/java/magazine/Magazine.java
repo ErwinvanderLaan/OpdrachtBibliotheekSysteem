d7c9ac2ef7e09ad77f0e66c2ef95a8a4948ce12d
Erwin van der Laan <erwinvanderlaan@gmail.com>
2024-11-21 18:33:44 +0100

    Improved 'Book' and 'Magazine' controller & repository classes

--- src/main/java/magazine/Magazine.java
+++ src/main/java/magazine/Magazine.java
@@ -1,8 +1,9 @@
-package magazine;
+package Magazine;
 
+import java.time.LocalDate;
 import library.Item;
 
-public class Magazine extends Item {
+class Magazine extends Item {
 
   private String issn;
   private int issueNumber;
@@ -12,6 +13,23 @@ public class Magazine extends Item {
   public Magazine( long id, String title, String author, int pages, String issn, int issueNumber) {
    super(id, title, author, pages);
     this.issn = issn;
+    this.issueNumber = issueNumber;
+  }
+
+  // Getters & Setters
+  public String getIssn() {
+    return issn;
+  }
+
+  public int getIssueNumber() {
+    return issueNumber;
+  }
+
+  public void setIssn(String issn) {
+    this.issn = issn;
+  }
+
+  public void setIssueNumber(int issueNumber) {
     this.issueNumber = issueNumber;
   }
 
